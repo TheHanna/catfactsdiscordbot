b1b06632841fb743e589972e07f04bcfb4a0194b
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-08-04 17:36:10 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const facts = require('cat-facts');
+const client = new Discord.Client();
+
+client.login("MzQzMTUwNzE5NjA1NDA3NzU3.DGaAqQ.tZX6S9ttJLzUo4dqNfqewWXZGs4");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("catfacts")) {
+    message.channel.send(facts.random());
+    setInterval(() => {
+        message.channel.send(facts.random());
+    }, 5000);
+  }
+});
