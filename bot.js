5ab0fdc703c350b94584649fad84336708a9a1db
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-08-04 20:19:34 -0500

    fix that fuck up

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) {
+const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Welcome to CatFacts\n');
