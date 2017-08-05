44bd85780481752165401a688cb05cf9446c8337
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-08-04 20:18:39 -0500

    use an http server to keep the web proc alive

--- bot.js
+++ bot.js
@@ -1,3 +1,16 @@
+const http = require('http');
+const hostname = process.env.HOSTNAME || '127.0.0.1';
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Welcome to CatFacts\n');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}`)
+});
+
 const Discord = require('discord.js');
 const facts = require('cat-facts');
 const hook = new Discord.WebhookClient('343165371626553355', 's6SIzYAddOXHCOCmDippF-ZiQDTVJLm7qbx1ZhD4EPIbxc7uI0dxQRYoNU7OQovMx-_B');
